2865399fded51968d67f7d410213983ca25d3404
github username <github email address>
2020-01-10 19:54:14 -0700

    tried implementing nav, send up router/index.js but receiving error

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Timoun Kontan</h1>
+    <h3> "little by little the bird builds its nest"</h3>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
